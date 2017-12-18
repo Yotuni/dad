41a8cb0da508b58ad41297c043fdba5dcaae0f79
Yotuni <xantinho.tiago2@gmail.com>
2017-12-18 21:25:30 +0000

    Minor fix update
    Minor fixes to game controller

--- laravel/app/Http/Controllers/GameController.php
+++ laravel/app/Http/Controllers/GameController.php
@@ -38,7 +38,7 @@ class GameController extends Controller
         $game_user->game_id = $game->id;
         $game_user->user_id = $game->created_by;
         $game_user->save();
-        return redirect()->route('adminPanel.games.index')->with('success', 'Game added successfully!'); 
+        return redirect()->route('games.index')->with('success', 'Game added successfully!'); 
     }
 
     /**
@@ -103,6 +103,6 @@ class GameController extends Controller
     public function destroy($id)
     {
         Game::findOrFail($id)->delete();
-        return redirect()->route('adminPanel.games.index')->with('success', 'User deleted successfully!');
+        return redirect()->route('games.index')->with('success', 'User deleted successfully!');
     }
 }
